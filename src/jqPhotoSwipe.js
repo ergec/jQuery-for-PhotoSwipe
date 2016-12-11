89bfc13033acf9a153dc4b6e64dd8a703c6ae00d
ergec <ergecsenturk@gmail.com>
2016-12-11 19:47:20 +0300

    added forced gallery option

--- src/jqPhotoSwipe.js
+++ src/jqPhotoSwipe.js
@@ -5,19 +5,26 @@ https://ergec.github.io/jQuery-for-PhotoSwipe/
 (function (jQuery) {
 	jQuery.fn.jqPhotoSwipe = function (options) {
 		var _photoswipe = {};
-		var defaults = {};
+		var defaults = {
+			forceSingleGallery: false
+		};
 		_photoswipe.galleries = [];
 		_photoswipe.galleriesindex = [];
 		var $galleryid = 0;
 		var pswpHTML = '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"><div class="pswp__bg"></div><div class="pswp__scroll-wrap"><div class="pswp__container"><div class="pswp__item"></div><div class="pswp__item"></div><div class="pswp__item"></div></div><div class="pswp__ui pswp__ui--hidden"><div class="pswp__top-bar"><div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button><button class="pswp__button pswp__button--share" title="Share"></button><button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button><button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button><div class="pswp__preloader"><div class="pswp__preloader__icn"><div class="pswp__preloader__cut"><div class="pswp__preloader__donut"></div></div></div></div></div><div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"><div class="pswp__share-tooltip"></div> </div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button><button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button><div class="pswp__caption"><div class="pswp__caption__center"></div></div></div></div></div>';
 		var pswpElement = $(pswpHTML).appendTo("body")[0];
+		var forcedGalleryId = "forcedGallery" + (Math.random() * (9999999 - 1) + 1);
 		this.each(function () {
 			var $options = $.extend(defaults, options);
 			var $this = $(this);
 			var $galleryname = $this.data("fancybox-group");
 			if (!$galleryname) {
-				$galleryname = "singleimage" + (Math.random() * (9999999 - 1) + 1);
-				$this.data("fancybox-group", $galleryname);
+				if ($options.forceSingleGallery) {
+					$this.data("fancybox-group", forcedGalleryId);
+				}else {
+					$galleryname = "singleimage" + (Math.random() * (9999999 - 1) + 1);
+					$this.data("fancybox-group", $galleryname);
+				}
 			}
 			if (_photoswipe.galleriesindex.indexOf($galleryname) === -1) {
 				$galleryid = _photoswipe.galleriesindex.length;
